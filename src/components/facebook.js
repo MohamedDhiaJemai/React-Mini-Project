40cec1603ce1713f821005c39cba280d2422f3bb
ING. JEMAI M.Dhia <dhia.jemai@gmail.com>
2019-12-31 15:04:12 +0100

    Facebook Login Authentification

---
+++ src/components/facebook.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login';
+
+class facebook extends Component {
+
+    state = {
+        isLoggedIn: false,
+        userID: "",
+        name: "",
+        email: "",
+        picture: ""
+      };
+
+      responseFacebook = response => {
+        // console.log(response);
+    
+        this.setState({
+          isLoggedIn: true,
+          userID: response.userID,
+          name: response.name,
+          email: response.email,
+          picture: response.picture.data.url,
+        });
+      };
+
+      componentClicked = () => console.log("clicked");
+
+    render() {
+
+        let fbContent;
+
+        if(this.state.isLoggedIn){
+            fbContent = (
+                <div
+                  style={{
+                    width: "400px",
+                    margin: "auto",
+                    
+                    padding: "20px"
+                  }}
+                >
+                  
+                  <h2>Welcome {this.state.name}</h2>
+                  <p><img src={this.state.picture} alt={this.state.name} /></p>
+                  Email: {this.state.email}
+                </div>
+              );
+        } else{
+            fbContent = ( 
+            <FacebookLogin
+                appId="593111474806339"
+                autoLoad={true}
+                fields="name,email,picture"
+                onClick={this.componentClicked}
+                callback={this.responseFacebook}
+            />
+            );
+        }
+
+        return <div>{fbContent}</div>;
+    }
+}
+
+export default facebook;
